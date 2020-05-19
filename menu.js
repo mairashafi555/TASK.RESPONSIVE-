7afb456128112e91d215ef01728487e13eca7cea
mairashafi555 <mairashafi555@gmail.com>
2020-05-19 21:40:37 +0500

    all images and files req for webpage

---
+++ menu.js
@@ -0,0 +1,13 @@
+// $(document).ready(function(){
+//     $(".burger".on("click", function(){
+//         $("header nav ul").toggleClass("open");
+//     }
+// })
+
+// $(document).ready(function(){
+//     $(".burger".on("click", function(){
+//         $(".navs li").toggleClass("open");
+//     }
+// })
+
+
